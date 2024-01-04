987c86bcf7b01681c12ab0a7065a640c81381b10
noman <nomannasiri5@gmail.com>
2024-01-04 13:53:36 +0500

    configed the layout for organization page

---
+++ app/(platfrom)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -0,0 +1,10 @@
+const organizationPage = ({children}: {children : React.ReactNode}) => {
+return(
+    <main className="pt-20 md:pt-24 px-4 max-w-6xl" >
+        <div>
+            {children}
+        </div>
+    </main>
+)
+}
+export default organizationPage
--- components/logo.tsx
+++ components/logo.tsx
@@ -9,7 +9,7 @@ const Logo = () => {
             <div className={cn("hover:opacity-70 transition items-center gap-x-2 hidden md:flex", textFont.className)}>
                 <Image
                     alt="logo"
-                    src="/logo.svg"
+                    src="/vercel.svg"
                     height={40}
                     width={40} />
                 <p className="text-lg text-neutral-700 pb-1">Gapshap</p>
