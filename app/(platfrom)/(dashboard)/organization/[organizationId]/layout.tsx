a66b9fc30492d5fcb2c14099cd397635135dcc56
Noman <nomannasiri5@gmail.com>
2024-01-05 05:52:36 -0800

    made changes on organization page

--- app/(platfrom)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platfrom)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,7 +1,10 @@
-const organizationPage = ({children}: {children : React.ReactNode}) => {
+const OrganizationPage = ({children}: {children : React.ReactNode}) => {
 return(
-    <main className="pt-20 md:pt-24 px-4 max-w-6xl" >
-        <div>
+    <main className="pt-20 md:pt-24 px-4 max-w-6xl 2xl:max-w-screen-xl mx-auto" >
+        <div className="flex gap-x-7">
+            <div className="w-64  shrink-0 hidden md:block">
+{/* sidebar */}
+            </div>
             {children}
         </div>
     </main>
@@ -9,4 +12,4 @@ return(
 
         
 }
-export default organizationPage
+export default OrganizationPage
